e25d9e893f65878f9b1051afcd00a8154a374fe2
Ikram Ico <ikramicono@gmail.com>
2024-02-28 08:51:56 +0600

    switch

---
+++ script.js
@@ -0,0 +1,57 @@
+let a = '16';
+const b=36;
+/*console.log(a,b)
+
+console.log(a,b);
+if(typeof b =='number'&& b>30){
+    console.log('pass')
+}
+else{
+    console.log('fail');
+}
+if( typeof a =='string' || a>10){
+    console.log('or')
+}
+else{
+    console.log('er')
+}
+
+
+
+
+
+
+
+
+
+if(a===b){
+    console.table('a is number');
+}
+else{
+    console.table('a is string');
+}
+// if(typeof a =='string'){
+//     console.log(typeof a);
+// }
+
+
+if(a<16){
+    console.log('a is less than 16');
+}
+else if(a>16){
+    console.log('a is greater than 16')
+}
+else{console.log('a is equal to 16');}
+*/
+
+/*switch(true){
+
+    case (b>0 && b<10):
+        console.log('b is less than 10');
+        break;
+    case (b>10 && b<20):
+        console.log('b is 16');
+        break;
+    default: console.log('b is greater than 20');
+    break;
+}*/
