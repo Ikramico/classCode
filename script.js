e8ea54c80fc81d0a3c027f9ce4eea40d58788bc2
Ikram Ico <ikramicono@gmail.com>
2024-02-29 09:43:35 +0600

    loop

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let a = '16';
+let a = 1;
 const b=36;
 /*console.log(a,b)
 
@@ -55,9 +55,23 @@ else{console.log('a is equal to 16');}
     default: console.log('b is greater than 20');
     break;
 }*/
-for(let i=0; i<22; i++){
-    
-    
-    
+/*for(let i=0; i<22; i++){
+    console.log(i);
 }
 
+while(a<22){
+    a++;
+    console.log(a);
+}*/
+
+/*do{
+    a++;
+    console.log(a);
+}while(a<20);*/
+/*for(let i=0; i<10; i++){
+    for(let j=0; j<=i; j++){
+        console.log(j);
+    }
+    console.log(``);
+}
+console.log(a);*/
